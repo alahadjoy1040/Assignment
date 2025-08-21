725f9a4b0f3533021439960903c773aa15f4a7ca
alahadjoy1040 <alahadjoy08@gmail.com>
2025-08-21 20:48:14 +0600

    006multiply

---
+++ 006multiply.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x, y;
+    cout << "Enter two floating numbers: ";
+    cin >> x >> y;
+    cout << "Product = " << x * y;
+    return 0;
+}
