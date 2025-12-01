ea8c5fe9b174db64ac73f11194fc3e0e8d7afcc1
alahadjoy1040 <alahadjoy08@gmail.com>
2025-12-01 22:13:44 +0600

    040_180_rotation_of_simple_pyramid.cpp

---
+++ 040_180_rotation_of_simple_pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = n; i >= 1; i--) {
+        for (int s = 1; s <= n - i; s++) cout << " ";
+        for (int j = 1; j <= 2 * i - 1; j++) cout << "*";
+        cout << "\n";
+    }
+    return 0;
+}
+
